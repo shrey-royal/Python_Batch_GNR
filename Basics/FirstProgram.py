a27c0f9d56b978c0cdbb778f9744eb12d2ee59d2
Shrey K <srkkadia@gmail.com>
2024-07-31 21:01:16 +0530

    Intrduction to Python Language

---
+++ Basics/FirstProgram.py
@@ -0,0 +1,64 @@
+# single line comment
+'''
+multi
+line
+comment
+'''
+"""
+multi
+line
+comment
+"""
+'''
+print statement:
+syntax:
+    (*objects, sep=' ', end="\n") -> None
+'''
+
+# print("Hello, World!", end='')
+# print("This is an object", '\t', "this is another object", 123, '\n', 'this is the third object')
+
+# print("Apple", "Banana", "Cherry", sep='\t') # sep is seperator which will seperate the objects
+# print("Apple", "Banana", "Cherry", sep='\t', end='\\') # end 
+
+# Data Types
+"""
+
+1. Text: str
+2. Numeric: int, float, complex(117j)
+3. Sequence: list, tuple, range
+4. Mapping: dict
+5. Set: set, frozenset
+6. Boolean: bool
+7. Binary: bytes(binary), bytearray(array of binary nums)
+8. None: NoneType
+"""
+
+# print("\"Hello, World!\"")
+print("""Hello 
+      wedsiofhweruibgjkeruig
+      rth
+      rt
+      hrty
+      hrty
+      hj
+      rhj
+      hrty
+      hjt
+      j
+      tj
+      tyu
+      World!""")
+print("\"\"\"Hello World!\"\"\"")
+
+print("dergedfg"+
+      "dsg"+
+      "dfsg"+
+      "sdfg"+
+      'dsf'+
+      'globalgd'+
+      "FloatingPointErrordfg"+
+      'defgdf'+
+      'g')
+
+# id, type
