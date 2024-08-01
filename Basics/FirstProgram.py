5ed408de76d7e627a727fa33d2b5b000ba9d4007
Shrey K <srkkadia@gmail.com>
2024-08-01 21:01:11 +0530

    Basics, Operators

--- Basics/FirstProgram.py
+++ Basics/FirstProgram.py
@@ -35,30 +35,62 @@ syntax:
 """
 
 # print("\"Hello, World!\"")
-print("""Hello 
-      wedsiofhweruibgjkeruig
-      rth
-      rt
-      hrty
-      hrty
-      hj
-      rhj
-      hrty
-      hjt
-      j
-      tj
-      tyu
-      World!""")
-print("\"\"\"Hello World!\"\"\"")
+# print("""Hello 
+#       wedsiofhweruibgjkeruig
+#       rth
+#       rt
+#       hrty
+#       hrty
+#       hj
+#       rhj
+#       hrty
+#       hjt
+#       j
+#       tj
+#       tyu
+#       World!""")
+# print("\"\"\"Hello World!\"\"\"")
 
-print("dergedfg"+
-      "dsg"+
-      "dfsg"+
-      "sdfg"+
-      'dsf'+
-      'globalgd'+
-      "FloatingPointErrordfg"+
-      'defgdf'+
-      'g')
+# print("dergedfg"+
+#       "dsg"+
+#       "dfsg"+
+#       "sdfg"+
+#       'dsf'+
+#       'globalgd'+
+#       "FloatingPointErrordfg"+
+#       'defgdf'+
+#       'g')
 
 # id, type
+# id() -> used to verify the identity of any variable
+# type() -> used to identify the data type (return classes)
+
+# Variables
+
+# a = "This is String"
+# print(a, id(a), sep=' -> ')
+
+# b = a
+# print(a, b, id(a), id(b), sep='\n')
+# a = "abcd"
+# b = "xyz"
+# print(a, b, id(a), id(b), sep='\n')
+
+# c = "This is String"
+# print(c, id(c), sep=' -> ')
+
+
+# a = "strr"
+# print("a = ", a, "\nid = ", id(a), "\ntype = ", type(a))
+# print("a = " + str(a) + "\nid = " + str(id(a)) + "\ntype = " + str(type(a)))
+# print(f"a = {a}\nid = {id(a)}\ntype = {type(a)}")
+
+# a = 12                # int
+# a = 12.2              # float
+# a = 1-12j             # complex
+# a = "name"            # str
+# a = True  # False     # bool (True/False)
+# a = b'01'             # bytes
+a = None                # None
+
+print(f"{id(a)} -> {type(a)} -> {a}")
---
+++ Basics/Operators.py
@@ -0,0 +1,33 @@
+# Arithmetic operators
+# print(2 ** 3) # Exponentiation
+# print(16 // 2) # floor division
+# Assignment operators
+# Comparison operators
+# Logical operators -> and, or, not
+# Identity operators -> is, is not
+    # -> it checks if both variables points at same object or different objectes in memory
+# Membership operators -> in, not in
+    # -> check if the value is present or absent in a collection-like object
+# Bitwise operators
+
+
+# print(1+2)
+# print('1'+'2')
+
+# print(1-2)
+
+# print(12*3)
+# print('12 '*3)
+
+# print(12/3)
+
+# print(2 ** 3 ** 2 * 2)
+
+# a = 123
+# b = a
+# print(a is b)
+
+# a = "Shrey"
+# print("Shrey" is not str)
+
+# print(1 not in (11, 2, 3))
