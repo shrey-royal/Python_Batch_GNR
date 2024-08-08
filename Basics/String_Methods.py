9578ad9ff21c0ba3ae5357c812d96cb4d42098fa
Shrey K <srkkadia@gmail.com>
2024-08-08 21:14:39 +0530

    String Methods 0

---
+++ Basics/String_Methods.py
@@ -0,0 +1,7 @@
+text = "helLo World!"
+# print(len(text))
+# print(text.capitalize())
+# print(text.casefold() == "hello world!")
+# print(text.count('l'))
+# print(text.center(len(text)+10, '-'))
+# print('cāf𐌄'.encode(encoding='ASCII', errors='xmlcharrefreplace'))
