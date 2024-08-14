050950feb155e772ced132d80d7bd7c826097c22
Shrey K <srkkadia@gmail.com>
2024-08-14 20:54:55 +0530

    String methods: e->i

--- Basics/String_Methods.py
+++ Basics/String_Methods.py
@@ -1,7 +1,21 @@
-text = "helLo World!"
+# text = "helLo World!"
 # print(len(text))
 # print(text.capitalize())
 # print(text.casefold() == "hello world!")
 # print(text.count('l'))
 # print(text.center(len(text)+10, '-'))
 # print('cāf𐌄'.encode(encoding='ASCII', errors='xmlcharrefreplace'))
+
+# text = "python programming is fun"
+# print(text.endswith('is fun'))
+# print("Hello\tWorld".expandtabs(10))
+
+# print("hello world".find('o', 0, 3))
+# print("python programming".find('prog'))  # returns -1
+
+# text = "Good {}!"
+# print(text.format("Evening"))
+# print("Addition of {} and {} is {}".format(5, 3, 5+3))
+# print("{2}, {0}, {3} and {1} are learing python with me.".format("Archi", "Herry", "Dhruv", "Yug"))
+
+# print("hello world".index('_'))     # returns ValueError
