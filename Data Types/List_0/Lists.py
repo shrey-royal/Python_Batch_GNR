e67e90fbd83d3bffc3296451ee3ec96371f45dcd
Shrey K <srkkadia@gmail.com>
2024-09-16 14:56:52 +0530

    List Programs

--- Data Types/List_0/Lists.py
+++ Data Types/List_0/Lists.py
@@ -168,3 +168,13 @@ pprint(indian_veggies)
 #             print(elem[j])
 #     else:
 #         print(elem)
+
+"""
+Programs:
+
+1. Write a Python program to get the largest number from a list.
+2. Write a Python program to get the smallest number from a list.
+3. Write a Python program to count the number of strings from a given list of strings. The string length is 2 or more and the first and last characters are the same.
+Sample List : ['abc', 'xyz', 'aba', '1221']
+Expected Result : 2
+"""
